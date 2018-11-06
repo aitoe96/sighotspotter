ca995242373046fb4af67fdd90379dd3189128be
Andras Hartmann <andras.hartmann@uni.lu>
2018-11-06 10:41:43 +0100

    Some more documentation

---
+++ R/NicheSIG.R
@@ -0,0 +1,7 @@
+#' NicheSIG: A computational approach to control cell phenotype for cellular rejuvenation strategies
+#'
+#' This package introduces a novel approach to infer the effect of the cellular environment (niche) on the cell signaling cascade
+#'
+#' Markov Chain for Cell Signaling Cascade Inference
+#'
+"_PACKAGE"
